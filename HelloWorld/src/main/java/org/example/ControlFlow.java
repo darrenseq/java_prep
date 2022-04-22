78b59915fb0b63c265c3058c11065ab43d85ad28
darrenseq <darren.sequeira.ds@gmail.com>
2022-04-22 09:40:36 -0400

    flow control

--- HelloWorld/src/main/java/org/example/ControlFlow.java
+++ HelloWorld/src/main/java/org/example/ControlFlow.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class ControlFlow {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
 //        int temp = 12;
 //        boolean isWarm = temp > 20 && temp < 30;
 //        System.out.println(isWarm);
@@ -10,16 +14,36 @@ public class ControlFlow {
 //        boolean hasCriminalRecord = false;
 //        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
 //        System.out.println(isEligible);
-        String role = "admin";
-        switch (role) {
-            case "admin":
-                System.out.println("You are an Admin");
-                break;
-            case "moderator":
-                System.out.println("You are a moderator");
-                break;
-            default:
-                System.out.println("You are a guest");
+
+//        Break and continue
+//        while(true){
+//            System.out.println("Input: ");
+//            input = scanner.next().toLowerCase();
+//            if(input.equals("pass"))
+////                moves control to beginning of the loop
+//                continue;
+//            if(input.equals("quit"))
+////                break takes control out of the loop
+//                break;
+//            System.out.println(input);
+//        }
+//
+//        Foreach
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for (String fruit: fruits) {
+            System.out.println(fruit);
         }
+//        String role = "admin";
+//        switch (role) {
+//            case "admin":
+//                System.out.println("You are an Admin");
+//                break;
+//            case "moderator":
+//                System.out.println("You are a moderator");
+//                break;
+//            default:
+//                System.out.println("You are a guest");
+//        }
     }
 }
--- HelloWorld/src/main/java/org/example/MortgageCalculator.java
+++ HelloWorld/src/main/java/org/example/MortgageCalculator.java
@@ -7,24 +7,41 @@ public class MortgageCalculator {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
+        int principal = 0;
+        float annualInterest  = 0;
+        byte years = 0;
 
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1,000 & 1,000,000");
+        }
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30)
+                break;
+            System.out.println("Enter year between 1 and 30");
+        }
         int numberOfPayments = years * MONTHS_IN_YEARS;
 
         double power = Math.pow((1 + monthlyInterest), numberOfPayments);
         double mortgage = principal * (monthlyInterest * power) / ( power - 1);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: "+mortgageFormatted);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
